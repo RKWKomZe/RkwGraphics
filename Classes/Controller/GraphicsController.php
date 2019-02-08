49ac735a29e65314ac49fa4e1d5c47965e86b09c
Steffen kroggel <kroggel@rkw.de>
2019-02-08 15:59:07 +0100

    [ADD] Inital commit

---
+++ Classes/Controller/GraphicsController.php
@@ -0,0 +1,44 @@
+<?php
+namespace RKW\RkwGraphics\Controller;
+
+/***
+ *
+ * This file is part of the "RKW Graphics" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ *  (c) 2019 Steffen Kroggel <developer@steffenkroggel.de>
+ *
+ ***/
+
+/**
+ * GraphicsController
+ */
+class GraphicsController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
+{
+
+
+
+    /**
+     * action pie
+     *
+     * @return void
+     */
+    public function pieAction()
+    {
+
+    }
+
+    /**
+     * action bars
+     *
+     * @return void
+     */
+    public function barsAction()
+    {
+
+    }
+
+
+}
